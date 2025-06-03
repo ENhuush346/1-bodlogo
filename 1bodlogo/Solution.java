6d880737e943b35f628c0490155ec8b844afdbd4
ENhuush346 <sakask12331@gmail.com>
2025-06-03 14:10:32 +0800

    Create Solution.java

---
+++ 1bodlogo/Solution.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class Solution {
+    public static long getWays(int n, int[] coins) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] coins = new int[m];
+        for (int i = 0; i < m; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        System.out.println(getWays(n, coins));
+    }
+}
