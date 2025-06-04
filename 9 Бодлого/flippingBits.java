2a2e5042611144804488a8e79b108889ec5dfa11
Энхбаяр <sakask12331@gmail.com>
2025-06-04 14:59:51 +0800

    Create flippingBits.java

---
+++ 9 Бодлого/flippingBits.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+    public static long flippingBits(long n) {
+        long allOnes = 0xFFFFFFFFL;  // 32 битийн бүх бит 1
+        return allOnes - n;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
