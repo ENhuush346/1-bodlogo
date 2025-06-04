5d00ae8d73c61a295507d642de74f97b5b4af98d
Энхбаяр <sakask12331@gmail.com>
2025-06-04 14:37:49 +0800

    Update hackerlandRadioTransmitters.java

--- 6 Бодлого /hackerlandRadioTransmitters.java
+++ 6 Бодлого /hackerlandRadioTransmitters.java
@@ -1,7 +1,15 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
 import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
 
 class Result {
     public static int hackerlandRadioTransmitters(List<Integer> x, int k) {
