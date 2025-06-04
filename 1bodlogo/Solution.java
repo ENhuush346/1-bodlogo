af6b6e45a40562227bd65a0a3128346022923d36
Энхбаяр <sakask12331@gmail.com>
2025-06-04 14:37:13 +0800

    Update Solution.java

--- 1bodlogo/Solution.java
+++ 1bodlogo/Solution.java
@@ -1,28 +1,36 @@
-
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution {
-    public static long getWays(int n, int[] coins) {
+class Result {
+
+    public static long getWays(int n, List<Long> c) {
         long[] dp = new long[n + 1];
         dp[0] = 1;
 
-        for (int coin : coins) {
-            for (int i = coin; i <= n; i++) {
-                dp[i] += dp[i - coin];
+        for (long coin : c) {
+            for (int amount = (int)coin; amount <= n; amount++) {
+                dp[amount] += dp[amount - (int)coin];
             }
         }
 
         return dp[n];
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] coins = new int[m];
-        for (int i = 0; i < m; i++) {
-            coins[i] = scanner.nextInt();
-        }
-
-        System.out.println(getWays(n, coins));
-    }
 }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.par
