12e14bbc508204a1f47aaaed7590bded57881319
Энхбаяр <sakask12331@gmail.com>
2025-06-04 15:00:11 +0800

    Update flippingBits.java

--- 9 Бодлого/flippingBits.java
+++ 9 Бодлого/flippingBits.java
@@ -1,8 +1,15 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
     public static long flippingBits(long n) {
         long allOnes = 0xFFFFFFFFL;  // 32 битийн бүх бит 1
         return allOnes - n;
