e728dac518362a73cf789ffafb35eae6612345b5
Энхбаяр <sakask12331@gmail.com>
2025-06-04 14:30:50 +0800

    Create hackerlandRadioTransmitters.java

---
+++ 6 Бодлого /hackerlandRadioTransmitters.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int hackerlandRadioTransmitters(List<Integer> x, int k) {
+        Collections.sort(x);
+        int count = 0;
+        int i = 0;
+        int n = x.size();
+
+        while (i < n) {
+            count++;
+            int loc = x.get(i) + k;
+
+            while (i < n && x.get(i) <= loc) {
+                i++;
+            }
+
+            loc = x.get(i - 1) + k;
+
+            while (i < n && x.get(i) <= loc) {
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        // Хэрвээ hackerrank орчинд OUTPUT_PATH ашиглаж байгаа бол дараах мөрийг ашиглана:
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> x = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.hackerlandRadioTransmitters(x, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
